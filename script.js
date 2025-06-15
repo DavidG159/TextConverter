846e07e79642dd17d0b03a9f1b219ffce030ba27
DavidG159 <gaspardavid235@gmail.com>
2025-06-15 15:43:37 -0600

    testing functionality p1

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 'use strict';
 
+//variable
 const textInput = document.querySelector('.textmain');
 const convertBtn = document.querySelector('.convert');
 let textResult = document.querySelector('.textresult');
 
+//function
+function convertText(text) {
+
+    let newtxt;
+    const specialCharchecker = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(text);
+    const checkspace = /\s/.test(text);
+
+
+    if (checkspace) {
+        newtxt = text.trim();
+        specialCharchecker ? console.log('specialcharyes') : console.log('specialcharno');
+        console.log(newtxt);
+
+        // let test = newtxt = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/ ? console.log('real') : console.log('nope2');
+
+    } else {
+        console.log('nope');
+    }
+
+
+
+
+}
+
+//event listener
 convertBtn.addEventListener('click', function () {
 
-    const test = textInput.value;
-    textResult.textContent = test;
-    console.log(test);
+    const textHolder = textInput.value;
+    textResult.textContent = textHolder;
+    convertText(textHolder);
 
 
 
