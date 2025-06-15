5a5ac6aa4e1f02831ba99d050e9d5097002f9c62
DavidG159 <gaspardavid235@gmail.com>
2025-06-15 15:07:42 -0600

    testing functionality of textresult

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const textInput = document.querySelector('.textmain');
+const convertBtn = document.querySelector('.convert');
+let textResult = document.querySelector('.textresult');
+
+convertBtn.addEventListener('click', function () {
+
+    const test = textInput.value;
+    textResult.textContent = test;
+    console.log(test);
+
+
+
+});
+
