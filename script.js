550b329f9ac31a43c69228862605b9dd871cd89e
DavidG159 <gaspardavid235@gmail.com>
2025-06-16 20:57:20 -0600

    testing functionality p3

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ function convertText(text) {
 
     let newtxt;
     let newt;
+    let makeArray;
     const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g; // specialchar
     const specialCharchecker = specialChars.test(text);  // checking if it has specialchars - returns true or false
     const checkspace = /\s/.test(text);                 // checking if it has space - returns true or false
 
 
-
+    //checks if the start and end of the string has a space
     if (checkspace) {
 
-        newtxt = text.trim();// 
+        newtxt = text.trim();// removes space on the start and at the end of the string 
         specialCharchecker ? newt = text.split(specialChars) : console.log('specialcharno');
-        console.log(newt);
+        makeArray = new Array(newt.length);
+        console.log(newt.length);
 
         // let test = newtxt = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/ ? console.log('real') : console.log('nope2');
 
